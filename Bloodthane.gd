4ccc02d88fe7481fbf59b3995a9c98a9027f1100
kultcher <68703333+kultcher@users.noreply.github.com>
2020-07-25 16:11:46 -0400

    Add files via upload

---
+++ Bloodthane.gd
@@ -0,0 +1,7 @@
+extends "res://Actor.gd"
+
+func start_new_turn():
+	actions = max_actions
+
+func populate_skills():
+	pass
---
+++ GameController.gd
@@ -0,0 +1,27 @@
+extends Node
+
+enum {PLAYER_TURN, ENEMY_TURN}
+enum {NO_SELECTION, PLAYER_MOVE, PLAYER_ACTION, ENEMY_MOVE, ENEMY_ACTION}
+
+var turn
+var state = NO_SELECTION
+
+func start_player_turn():
+	turn = PLAYER_TURN
+	state = NO_SELECTION
+	get_tree().call_group("all_player_units", "start_new_turn")
+
+	
+	
+func end_player_turn():
+	print("Player ended turn.")
+#	get_tree().call_group("checked_tiles", "deselect")
+	$TurnTimer.start()
+	yield($TurnTimer, "timeout")
+	start_enemy_turn()
+
+
+func start_enemy_turn():
+	turn = ENEMY_TURN
+	state = NO_SELECTION
+	$World/Encounter.start_turn()
